8e1cb38896c12031c464e39c603d5fe3bb671db9
hi2gage <hi2gage.pdx@gmail.com>
2023-12-31 18:51:23 -0700

    Add new key mapping for adding spaces above and below

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Blank Lines
+-- Map <Enter> to open a new line below and enter insert mode
+vim.keymap.set("n", "<CR>", "o<ESC>", { noremap = true, silent = true })
+
+-- Map <S-Enter> to open a new line above and enter insert mode
+vim.keymap.set("n", "<S-CR>", "O<ESC>", { noremap = true, silent = true })
