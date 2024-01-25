adcd1551506b165e8e6c5e4796d99315505edc94
hi2gage <hi2gage.pdx@gmail.com>
2024-01-25 02:30:44 -0700

    Everything is working

--- lua/gage/keymaps.lua
+++ lua/gage/keymaps.lua
@@ -7,14 +7,22 @@ vim.g.maplocalleader = " "
 
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
-
 -- Insert Mode --
 -- JK to go exit Insert Mode
 keymap("i", "jk", "<ESC>", opts)
-
-
 
 -- Visual Mode --
 
 keymap("v", ">", ">gv", opts)
 keymap("v", "<", "<gv", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", "<cmd> Telescope find_files <CR>", opts)
+-- keymap("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>", opts)
+-- keymap("n", "<leader>fw", "<cmd> Telescope live_grep <CR>")
+-- keymap("n", "<leader>gt", "<cmd> Telescope git_status <CR>")
+
+-- bufferline, cycle buffers
+keymap("n", "<Tab>", "<cmd> BufferLineCycleNext <CR>")
+keymap("n", "<S-Tab>", "<cmd> BufferLineCyclePrev <CR>")
+keymap("n", "<C-q>", "<cmd> bd <CR>")
