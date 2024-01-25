5ff5007d579ab5ab690173242a3e59d39243eeeb
hi2gage <hi2gage.pdx@gmail.com>
2024-01-24 23:57:02 -0700

    Change tabs to 4 spaces and add keymaps

--- lua/gage/keymaps.lua
+++ lua/gage/keymaps.lua
@@ -1,10 +1,20 @@
 local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true 
+local opts = { noremap = true, silent = true }
 
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
+
+-- Insert Mode --
+-- JK to go exit Insert Mode
+keymap("i", "jk", "<ESC>", opts)
+
+
+
+-- Visual Mode --
+
+keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts)
